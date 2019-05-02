4482ff5cc459d1ac5b5bee7985b72d5e36ecc1f2
Rafael C Antunes <RafaelcAntunes3@gmail.com>
2019-05-02 00:30:26 -0300

    Add Class ItemPedido

---
+++ prjPOOMayconRafael/src/fatec/poo/model/ItemPedido.java
@@ -0,0 +1,28 @@
+package fatec.poo.model;
+import fatec.poo.model.Produto;
+
+public class ItemPedido {
+    private int sequencia;
+    private double qtdeVendida;
+    private Produto produto;
+
+    public ItemPedido(int sequencia, double qtdeVendida, Produto produto) {
+        this.sequencia = sequencia;
+        this.qtdeVendida = qtdeVendida;
+        this.produto = produto;
+    }
+
+    public void setQtdeVendida(double qtdeVendida) {
+        this.qtdeVendida = qtdeVendida;
+    }
+
+    public int getSequencia() {
+        return sequencia;
+    }
+
+    public double getQtdeVendida() {
+        return qtdeVendida;
+    }
+    
+        
+}
