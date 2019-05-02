9a6dc8d75f0980d9f8df779f8cc96cad5b632808
Maycon William Soler <mayconwsoler@gmail.com>
2019-05-02 00:32:35 -0300

    Updt Vendedor (Gets,Sets)

--- prjPOOMayconRafael/src/fatec/poo/model/Vendedor.java
+++ prjPOOMayconRafael/src/fatec/poo/model/Vendedor.java
@@ -1,5 +1,27 @@
 package fatec.poo.model;
 
-public class Vendedor {
-    
+public class Vendedor extends Pessoa{
+    double salarioBase, taxaComissao;
+
+    public Vendedor(String cpf, String nome, double salarioBase) {
+        super(cpf, nome);
+        this.salarioBase = salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public void setTaxaComissao(double taxaComissao) {
+        this.taxaComissao = taxaComissao;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public double getTaxaComissao() {
+        return taxaComissao;
+    }
+        
 }
