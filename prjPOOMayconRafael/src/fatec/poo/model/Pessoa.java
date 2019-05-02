b2f54ce1fdec4e221934545da5ae7279a851c502
Maycon William Soler <mayconwsoler@gmail.com>
2019-05-01 23:56:43 -0300

    Adicionado Alterado Pessoa

--- prjPOOMayconRafael/src/fatec/poo/model/Pessoa.java
+++ prjPOOMayconRafael/src/fatec/poo/model/Pessoa.java
@@ -1,9 +1,42 @@
 package fatec.poo.model;
 
-/**
- *
- * @author mayco
- */
 public class Pessoa {
+    private String cpf,
+                   nome,
+                   endereco,
+                   cidade,
+                   uf,
+                   cep,
+                   ddd,
+                   telefone;
+
+    public Pessoa(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
     
 }
