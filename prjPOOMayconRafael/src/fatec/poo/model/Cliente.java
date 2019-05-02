956a2f290805a4179851ace6fdab34fb4e62efac
Maycon William Soler <mayconwsoler@gmail.com>
2019-05-02 00:54:33 -0300

    Updt Cliente,Vendedor (ArrayList Pedido)

--- prjPOOMayconRafael/src/fatec/poo/model/Cliente.java
+++ prjPOOMayconRafael/src/fatec/poo/model/Cliente.java
@@ -1,7 +1,10 @@
 package fatec.poo.model;
 
+import java.util.ArrayList;
+
 public class Cliente extends Pessoa {
     private double limiteCred,limiteDisp;
+    private ArrayList<Pedido> arrObjPedido = new ArrayList<Pedido>();
     
     public Cliente(String cpf, String nome, double limiteCred) {
         super(cpf, nome);
@@ -20,4 +23,7 @@ public class Cliente extends Pessoa {
         return limiteDisp;
     }
     
+    public void addPedido(Pedido pedido) {
+        this.arrObjPedido.add(pedido);
+    }
 }
