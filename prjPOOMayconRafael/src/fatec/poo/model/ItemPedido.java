7564e64b4caefe7e03588bc32eb78f781bceeffe
Rafael C Antunes <RafaelcAntunes3@gmail.com>
2019-05-02 01:25:24 -0300

    Add pointer to Pedido in ItemPedido and pointer to ItemPedido in Pedido

--- prjPOOMayconRafael/src/fatec/poo/model/ItemPedido.java
+++ prjPOOMayconRafael/src/fatec/poo/model/ItemPedido.java
@@ -1,15 +1,20 @@
 package fatec.poo.model;
 import fatec.poo.model.Produto;
 
+
 public class ItemPedido {
     private int sequencia;
     private double qtdeVendida;
     private Produto produto;
-
-    public ItemPedido(int sequencia, double qtdeVendida, Produto produto) {
+    private Pedido pedido;
+    
+    public ItemPedido(int sequencia, double qtdeVendida, Produto produto, Pedido pedido) {
         this.sequencia = sequencia;
         this.qtdeVendida = qtdeVendida;
         this.produto = produto;
+        this.pedido = pedido;
+        
+        this.produto.setQtdeEstoque(this.produto.getQtdeEstoque()-this.qtdeVendida);
     }
 
     public void setQtdeVendida(double qtdeVendida) {
--- prjPOOMayconRafael/src/fatec/poo/model/Pedido.java
+++ prjPOOMayconRafael/src/fatec/poo/model/Pedido.java
@@ -1,13 +1,23 @@
 package fatec.poo.model;
+import fatec.poo.model.ItemPedido;
+import java.util.ArrayList;
 
 
 public class Pedido {
  private String numero,dataEmissao,dataPagto;
  private boolean formaPagto,situacao;
-
-    public Pedido(String numero, String dataEmissao) {
+ private Cliente cliente;
+ private Vendedor vendedor;
+private ArrayList<ItemPedido> itemPedido = new ArrayList();
+private ItemPedido itemPedidoAux;
+ 
+ 
+    public Pedido(String numero, String dataEmissao, Cliente cliente, Vendedor vendedor, int sequencia, double qtdeVendida, Produto produto) {
         this.numero = numero;
         this.dataEmissao = dataEmissao;
+        this.cliente = cliente;
+        this.vendedor = vendedor;
+        addItemPedido(sequencia,qtdeVendida,produto,this);
     }
 
     public void setDataPagto(String dataPagto) {
@@ -42,7 +52,10 @@ public class Pedido {
         return situacao;
     }
  
- 
+    public final void addItemPedido(int sequencia, double qtdeVendida, Produto produto, Pedido pedido){
+        this.itemPedidoAux = new ItemPedido(sequencia,qtdeVendida,produto,pedido);
+        itemPedido.add(itemPedidoAux);
+    }
  
     
 }
